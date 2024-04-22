c0f332c2d57409bb5958832bcda9ffe8cbd43148
Francisco Cervantes <fjccandelario@gmail.com>
2024-04-22 17:29:13 -0600

    controladores y endpoints, como recuperar datos con params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,28 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('new')
+  newEnpoint(){
+    return 'Yo soy nuevo, sin slash';
+  }
+
+  @Get('/ruta/')
+  hello(){
+    return 'con slash'
+  }
+
+  @Get('/products/:id')
+  getProduct(@Param('id') id: string){
+    return `producto con id: ${id}`
+  }
+
+  @Get('/categories/:id/products/:productId')
+  getCategory(
+    @Param('id') id: string,
+    @Param('productId') productId: string
+  ){
+    return `categoria con id: ${id} y productId: ${productId}`;
   }
 }
