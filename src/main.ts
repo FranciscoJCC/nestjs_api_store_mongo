a48f7062132336a0429b6c192f90980eb8d20136
Francisco Cervantes <fjccandelario@gmail.com>
2024-04-24 20:04:28 -0600

    Se agrega validaciones para los data transfer object

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //Pipe, para validar inputs en las peticiones con los dtos
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, //Quita elementos del payload cuando no se define en el dto
+    forbidNonWhitelisted: true //Alertamos del error, cuando se agregue información no deseada al payload
+  }));
 
   await app.listen(3000);
 }
