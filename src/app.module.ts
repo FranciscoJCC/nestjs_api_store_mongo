d25deb6e3c9d5bf694b044fb5e610306b0c01c4a
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-01 16:06:26 -0600

    Configuración de variables de entorno, según el enviroment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -7,10 +8,19 @@ import { ProductsModule } from './products/products.module';
 import { firstValueFrom } from 'rxjs';
 import { DatabaseModule } from './database/database.module';
 
-
+import { enviroments } from 'enviroments';
 
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule, DatabaseModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    HttpModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV ] || '.env',
+      isGlobal: true,
+    })
+  ],
   controllers: [AppController],
   providers: [
     AppService,
