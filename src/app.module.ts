de3c3a896b5d8db2d9f9d76480fc3edd049b49cb
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-01 15:33:48 -0600

    uso de factory en el appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,31 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { firstValueFrom } from 'rxjs';
 
 const API_KEY = 'dev-123123123';
 const API_KEY_PROD = "prod-12343433232";
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, HttpModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async(http: HttpService) => {
+        const response = await firstValueFrom(http.get('https://jsonplaceholder.typicode.com/todos'));
+
+        return response.data;
+      },
+      inject: [ HttpService ]
     }
   ],
 })
