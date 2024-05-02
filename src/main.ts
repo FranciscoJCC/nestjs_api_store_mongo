ad2870aa3d0dd9614e2ad04c98a1177acb62a3ea
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-02 13:17:49 -0600

    Integración de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
     whitelist: true, //Quita elementos del payload cuando no se define en el dto
     forbidNonWhitelisted: true //Alertamos del error, cuando se agregue información no deseada al payload
   }));
+
+  const config = new DocumentBuilder()
+  .setTitle('Platzi Store')
+  .setDescription('The store API description')
+  .setVersion('1.0')
+  .addTag('store')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
